c6dfda321dc95a58e7b4028f02d2d4e472a3964e
anthony-aylward <aaylward@eng.ucsd.edu>
2019-05-21 02:19:20 -0700

    write app

--- okscope/__init__.py
+++ okscope/__init__.py
@@ -13,8 +13,7 @@ This file contains the application factory for the okscope app.
 # Imports ======================================================================
 
 import os
-
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 
 
@@ -44,17 +43,12 @@ def create_app(test_config=None):
         A flask app
     """
 
-    # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     
     if test_config is None:
-        # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
-        # load the test config if passed in
         app.config.from_mapping(test_config)
-    
-    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
@@ -62,10 +56,10 @@ def create_app(test_config=None):
     
     @app.route('/')
     def index():
-        return render_template('base.html')
-
-    @app.route('/okay')
-    def okay():
-        return 'everything is going to be okay'
+        return render_template('index.html', nav_button="About", url=url_for('about'))
+    
+    @app.route('/about')
+    def about():
+        return render_template('about.html', nav_button="Back", url=url_for('index'))
     
     return app
